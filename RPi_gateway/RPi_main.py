80202167d1017afacb647f0d1bd62133e1498eea
eSqadron_UbuntuRPi <kubam0906@gmail.com>
2023-04-16 09:00:57 +0200

    Longer sleep time before closing the door (RPI)

--- RPi_gateway/RPi_main.py
+++ RPi_gateway/RPi_main.py
@@ -117,7 +117,7 @@ class ShelfSense:
 
                     sleep(0.4)
 
-                sleep(2)
+                sleep(5)
                 self.send_to_unit(chr(0x18) + chr(lastContainerId))
 
 
@@ -178,7 +178,7 @@ class ShelfSense:
 
                     sleep(0.4)
 
-                sleep(2)
+                sleep(5)
                 self.send_to_unit(chr(0x18) + chr(lastContainerId))
 
 
